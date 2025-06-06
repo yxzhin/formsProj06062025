930de2958bca3bf632f33cb37fa1b595f243e795
yxzhin <yxzhin@gmail.com>
2025-06-06 18:36:27 +0200

    feat: add classes data and error

---
+++ data.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace theprj2
+{
+    public static class data
+    {
+
+        public static Dictionary<string, string> users = new Dictionary<string, string>();
+        public static string path = "users.txt";
+
+        public static short loadUsers()
+        {
+
+            if (string.IsNullOrEmpty(path))
+            {
+
+                error.show(-1);
+                return -1;
+
+            }
+
+            if (!File.Exists(path)) return 0;
+
+            string line, userName, password;
+
+            using(StreamReader sr = new StreamReader(path))
+            {
+
+                line = sr.ReadLine();
+
+                while(!string.IsNullOrEmpty(line))
+                {
+
+                    var splt = line.Split('|');
+
+                    if (splt.Length != 2) continue;
+
+                    (userName, password) = (splt[0], splt[1]);
+
+                    if (users.ContainsKey(userName)) continue;
+
+                    users.Add(userName, password);
+
+                }
+
+            }
+
+            return 1;
+
+        }
+
+        public static void saveUsers()
+        {
+
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+
+                foreach (var user in users) sw.WriteLine($"{user.Key}|{user.Value}");
+
+            }
+
+        }
+
+    }
+}
---
+++ error.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace theprj2
+{
+    public static class error
+    {
+
+        public static void show(short type)
+        {
+
+            switch (type)
+            {
+
+                case -1: MessageBox.Show("unesite validne vrednosti", "greska", MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+                case -2: MessageBox.Show("fajl nije pronadjen", "greska", MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+
+            }
+
+        }
+
+    }
+}
